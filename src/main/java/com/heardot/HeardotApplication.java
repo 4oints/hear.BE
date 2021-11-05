965cd2da97eaa7068c276d44181c9d2aeb685f7f
SDB016 <dongbin4013@naver.com>
2021-11-05 12:14:47 +0900

    feat: 라이브러리 세팅

---
+++ src/main/java/com/heardot/HeardotApplication.java
@@ -0,0 +1,13 @@
+package com.heardot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeardotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeardotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heardot/HeardotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heardot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeardotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
